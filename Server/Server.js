f03bce6a5741cae9463de47efe4e9eecb5cf830d
Sriram S <sriramshanmugam10@gmail.com>
2026-03-03 16:04:06 +0530

    Server

--- Server/Server.js
+++ Server/Server.js
@@ -16,14 +16,14 @@ const app = express();
 
  await connnectDB()
 
-app.use('/api/inngest', serve({ client:inngest, functions }));
+
 
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send('server is running'));
-
+app.use('/api/inngest', serve({ client:inngest, functions }));
 app.use('/api/user',userRouter)
 app.use('/api/post',postRouter)
 app.use('/api/story',storyRouter)
